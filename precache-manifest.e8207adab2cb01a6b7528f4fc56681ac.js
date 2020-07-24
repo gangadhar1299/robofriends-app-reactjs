7fb6483ab9efa140bbe016814b4456bc3b09fa40
Gangadhar N <gangadharn1299@gmail.com>
2020-07-24 21:22:02 +0530

    Updates

---
+++ precache-manifest.e8207adab2cb01a6b7528f4fc56681ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7079a8e27f6458fde299ce3a1373bfe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d042447e34d21820500",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "85a6c2bf800537103368",
+    "url": "/static/css/main.4a8df96c.chunk.css"
+  },
+  {
+    "revision": "5d042447e34d21820500",
+    "url": "/static/js/2.5bdd1b2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bdd1b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a6c2bf800537103368",
+    "url": "/static/js/main.5cb99e4c.chunk.js"
+  },
+  {
+    "revision": "a8bbf36ea5175c415e51",
+    "url": "/static/js/runtime-main.94f98e7f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
